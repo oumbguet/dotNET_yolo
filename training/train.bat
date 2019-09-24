48de3638ff3c635a2e81f7baf2a2419e5a46c217
Florian Millet <florian.mil68@gmail.com>
2019-09-24 11:39:51 +0200

    Add files via upload

--- training/train.bat
+++ training/train.bat
@@ -2,6 +2,4 @@ cd .\training
 move ..\train.txt .\
 move ..\test.txt .\
 move ..\Labels\output\* ..\Images\
-darknet.exe detector train ./cfg/obj.data ./cfg/obj.cfg ./darknet53.conv.74
-
-ping -n 6 127.0.0.1 >nul
+darknet.exe detector train ./cfg/obj.data ./cfg/obj.cfg ./darknet53.conv.74 %1
